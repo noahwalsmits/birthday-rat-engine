1348dd6834164832e862b652e808ac945df60424
Noah Walsmits <noahwalsmits@telfort.nl>
2020-03-15 22:49:49 +0100

    Updated info classes

---
+++ Java Game Engine/src/engine/EngineInfo.java
@@ -0,0 +1,6 @@
+package engine;
+
+public class EngineInfo {
+    public static final String NAME = "Birthday Rat Engine";
+    public static final String VERSION = "0.1";
+}
--- Java Game Engine/src/game/GameInfo.java
+++ Java Game Engine/src/game/GameInfo.java
@@ -4,5 +4,7 @@ package game;
  * Basic information and settings about the game can be changed here.
  */
 public class GameInfo {
-    public static String WINDOW_TITLE = "Example Game";
+    public static final String NAME = "Example Game";
+    public static final String VERSION = "0.0.0";
+    public static final String WINDOW_TITLE = GameInfo.NAME + " v" + GameInfo.VERSION;
 }

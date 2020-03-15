73b028fe56194b7fd6a83e1b885c281d47439778
Noah Walsmits <noahwalsmits@telfort.nl>
2020-03-15 22:54:11 +0100

    Changed engine version

--- Java Game Engine/src/engine/EngineInfo.java
+++ Java Game Engine/src/engine/EngineInfo.java
@@ -2,5 +2,5 @@ package engine;
 
 public class EngineInfo {
     public static final String NAME = "Birthday Rat Engine";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "3.1";
 }
